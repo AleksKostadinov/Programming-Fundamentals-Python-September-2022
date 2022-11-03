ffef39210d281ee000f5753c524d3de01c6eece8
AleksKostadinov <kostadinov12@gmail.com>
2022-11-03 08:26:10 +0200

    Exercises

--- dictionaries_basic/stock.py
+++ dictionaries_basic/stock.py
@@ -1,11 +1,11 @@
 elements = input().split()
+products = input().split()
 store = {}
 for i in range(0, len(elements), 2):
     key = elements[i]
     value = elements[i + 1]
     store[key] = int(value)
 
-products = input().split()
 for product in products:
     if product in store:
         print(f'We have {store[product]} of {product} left')
